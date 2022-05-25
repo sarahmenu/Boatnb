a6644fdea779bbdf051908f8c331204af8dec717
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-05-25 22:42:10 +0200

    fixed routes + controller

--- app/controllers/owner/boats_controller.rb
+++ app/controllers/owner/boats_controller.rb
@@ -1,16 +1,4 @@
 class Owner::BoatsController < ApplicationController
 
-  # def new
-  #   @boat = Boat.new
-  # end
 
-  # def create
-  #   @boat = Boat.new(boat_params)
-
-  #   if @boat.save
-  #     redirect_to boats_path
-  #   else
-  #     render :new
-  #   end
-  # end
 end
