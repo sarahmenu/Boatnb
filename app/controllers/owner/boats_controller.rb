4275e2a4473a85581b93278a197cc2e90c0c9b7f
Sarah Menu <sarah.menu@gmail.com>
2022-05-25 16:41:42 +0200

    fix owner controller

--- app/controllers/owner/boats_controller.rb
+++ app/controllers/owner/boats_controller.rb
@@ -1,16 +1,16 @@
-class BoatsController < ApplicationController
+class Owner::BoatsController < ApplicationController
 
-  def new
-    @boat = Boat.new
-  end
+  # def new
+  #   @boat = Boat.new
+  # end
 
-  def create
-    @boat = Boat.new(boat_params)
+  # def create
+  #   @boat = Boat.new(boat_params)
 
-    if @boat.save
-      redirect_to boats_path
-    else
-      render :new
-    end
-  end
+  #   if @boat.save
+  #     redirect_to boats_path
+  #   else
+  #     render :new
+  #   end
+  # end
 end
